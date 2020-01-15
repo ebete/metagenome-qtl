fd738f82b28a3802b59624adf99c19c6c9397094
Thom Griffioen <t.griffioen@nioo.knaw.nl>
2020-01-15 10:41:48 +0100

    Use argparse for CLI usage

--- bed_stats.py
+++ bed_stats.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
+import argparse
 import csv
-import sys
+import logging
 from pathlib import Path
 
 
 def get_coverage_per_gene(bed_file):
+    logging.info("Reading alignment depths from %s ...", bed_file.name)
+
     print("orf", "avg_depth", "coverage", sep="\t")
 
     with bed_file.open("r", newline="") as f:
@@ -24,7 +27,7 @@ def get_coverage_per_gene(bed_file):
 
             if cur_gene != last_gene:
                 if total > 0:
-                    print(last_gene, f"{gene_nucl/total:.1f}", f"{covered/total:.5f}", sep="\t")
+                    print(last_gene, f"{gene_nucl / total:.1f}", f"{covered / total:.5f}", sep="\t")
                 covered = 0
                 total = 0
                 gene_nucl = 0
@@ -37,4 +40,15 @@ def get_coverage_per_gene(bed_file):
 
 
 if __name__ == "__main__":
-    get_coverage_per_gene(Path(sys.argv[1]))
+    parser = argparse.ArgumentParser(description="Get the per-contig coverage and average depths.")
+    parser.add_argument("bed_file", metavar="BED", help="Reference alignment depth BED file.")
+    args = parser.parse_args()
+
+    logging.basicConfig(format="[%(asctime)s] %(message)s", level=logging.INFO)
+
+    bed_file = Path(args.bed_file)
+
+    try:
+        get_coverage_per_gene(bed_file)
+    except (BrokenPipeError, KeyboardInterrupt) as ex:
+        pass
