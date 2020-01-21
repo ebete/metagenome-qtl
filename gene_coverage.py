9b96ca0371bf75b7c4910a4fde86c434d08b328c
Thom Griffioen <t.griffioen@nioo.knaw.nl>
2020-01-21 15:56:26 +0100

    Output total nucleotides and orf length

--- gene_coverage.py
+++ gene_coverage.py
@@ -39,11 +39,36 @@ def get_depths(bed_fname, contig_ids):
             end = int(cols[2])
             depth = int(cols[3])
 
-            if not contig_id in contig_ids:
+            if contig_id not in contig_ids:
                 continue
 
             d.setdefault(contig_id, list()).append((start, end, depth))
     return d
+
+
+def per_orf_statistics(orf_regions, depths):
+    logging.info("Writing average depth per ORF ...")
+
+    print("sample_id", "orf_id", "length", "total_mapped", "avg_depth", sep="\t")
+
+    for contig_id in orf_regions:
+        depth = depths[contig_id]
+        depth_idx = 0
+        depth_start, depth_end, depth_val = depth[depth_idx]
+        for orf_start, orf_end, orf_id in orf_regions[contig_id]:
+            total_nucleotides = 0
+            while orf_start > depth_end and depth_idx < len(depth):
+                depth_idx += 1
+                depth_start, depth_end, depth_val = depth[depth_idx]
+            while orf_end > depth_start and depth_idx < len(depth) - 1:
+                overlap = min(orf_end, depth_end) - max(orf_start, depth_start)
+                total_nucleotides += depth_val * overlap
+                depth_idx += 1
+                depth_start, depth_end, depth_val = depth[depth_idx]
+            avg_depth = 0
+            if total_nucleotides != 0:
+                avg_depth = total_nucleotides / (orf_end - orf_start)
+            print(bed_file.name, orf_id, orf_end - orf_start, total_nucleotides, f"{avg_depth:.2f}", sep="\t")
 
 
 if __name__ == "__main__":
@@ -57,25 +82,9 @@ if __name__ == "__main__":
     gff_file = Path(args.gff_file)
     bed_file = Path(args.bed_file)
 
-    orf_regions = get_orfs(gff_file)
-    depths = get_depths(bed_file, orf_regions.keys())
-
-    logging.info("Writing average depth per ORF ...")
-    print("sample_id", "orf_id", "avg_depth", sep="\t")
-    for contig_id in orf_regions:
-        depth = depths[contig_id]
-        depth_idx = 0
-        depth_start, depth_end, depth_val = depth[depth_idx]
-        for orf_start, orf_end, orf_id in orf_regions[contig_id]:
-            avg_depth = 0
-            while orf_start > depth_end and depth_idx < len(depth):
-                depth_idx += 1
-                depth_start, depth_end, depth_val = depth[depth_idx]
-            while orf_end > depth_start and depth_idx < len(depth) - 1:
-                overlap = min(orf_end, depth_end) - max(orf_start, depth_start)
-                avg_depth += depth_val * overlap
-                depth_idx += 1
-                depth_start, depth_end, depth_val = depth[depth_idx]
-            if avg_depth != 0:
-                avg_depth /= (orf_end - orf_start)
-            print(bed_file.name, orf_id, f"{avg_depth:.2f}", sep="\t")
+    try:
+        orf_regions = get_orfs(gff_file)
+        depths = get_depths(bed_file, orf_regions.keys())
+        per_orf_statistics(orf_regions, depths)
+    except (BrokenPipeError, KeyboardInterrupt):
+        pass
